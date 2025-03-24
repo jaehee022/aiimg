ff387469cac20eeeea355a4de165e27df040f7c4
jaehee baek <baik0joo@gmail.com>
2025-03-24 03:27:44 +0000

    구구단 만들기 2

---
+++ test.py
@@ -0,0 +1,8 @@
+# number = int(input("구구단의 단을 입력하세요: "))
+ s_number = input("구구단의 단을 입력하세요: ")
+print(f"{number}단:')
+# print(number, "단:")
+# number 값이 문자열이기 떄문에 숫자로 변경해서 연산
+number = int(number)
+result = number * 2
+print(result)
